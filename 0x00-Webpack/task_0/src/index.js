1160b7acd7064a1714064349605737db2811cd94
Nzubechukwu Ifechukwu <nzube.ifechukwu@gmail.com>
2023-07-24 18:51:50 +0100

    Add .\package.json, .\src\index.js and .\dist\index.html

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,9 @@
+import $ from "jquery";
+
+$(function () {
+  $("body").html(
+    "<p>Holberton Dashboard</p>\
+    <p>Dashboard data for the students</p>\
+    <p>Copyright - Holberton School</p>"
+  );
+});
