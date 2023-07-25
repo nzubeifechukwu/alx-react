c8342b1cd3331ca2a380737fbd53c593e7849081
Nzubechukwu Ifechukwu <nzube.ifechukwu@gmail.com>
2023-07-25 13:45:28 +0100

    Add task_1 files

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,20 @@
+import $ from "jquery";
+import _ from "lodash";
+
+let count = 0;
+
+function updateCounter() {
+  count++;
+  $("#count").text(`${count} clicks on the button`);
+}
+
+const btn = $("<button>Click here to get started</button>").on(
+  "click",
+  _.debounce(updateCounter)
+);
+
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append(btn);
+$("body").append("<p id='count'></p>");
+$("body").append("<p>Copyright - Holberton School</p>");
