fff3502186c0bdd255fadf4181242bca2b421f51
Nzubechukwu Ifechukwu <nzube.ifechukwu@gmail.com>
2023-07-25 14:15:52 +0100

    .

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -8,13 +8,13 @@ function updateCounter() {
   $("#count").text(`${count} clicks on the button`);
 }
 
-const btn = $("<button>Click here to get started</button>").on(
+const $btn = $("<button>Click here to get started</button>").on(
   "click",
   _.debounce(updateCounter, 500, { leading: true, trailing: false })
 );
 
 $("body").append("<p>Holberton Dashboard</p>");
 $("body").append("<p>Dashboard data for the students</p>");
-$("body").append(btn);
+$("body").append($btn);
 $("body").append("<p id='count'></p>");
 $("body").append("<p>Copyright - Holberton School</p>");
