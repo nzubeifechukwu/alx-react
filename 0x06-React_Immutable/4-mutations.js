5e20ffea9ad46429c2966501028f048489bccdf2
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-08-26 19:43:57 +0100

    Update 4-mutations.js

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,6 +1,6 @@
 import { Map } from 'immutable';
 
-const map = Map({
+export const map = Map({
   1: 'Liam',
   2: 'Noah',
   3: 'Elijah',
@@ -9,5 +9,5 @@ const map = Map({
   6: 'Lucas',
 });
 
-const map2 = map.set(2, 'Benjamin').set(4, 'Oliver');
+export const map2 = map.set(2, 'Benjamin').set(4, 'Oliver');
 
