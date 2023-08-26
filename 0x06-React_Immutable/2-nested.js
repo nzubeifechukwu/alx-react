0c66b44d734c694c14f46ddcac9bb5c359aab86e
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-08-26 18:58:54 +0100

    Update 2-nested.js

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,6 +1,6 @@
 import { fromJS } from 'immutable';
 
-export default function accessImmutableObject(object, array) {
+export default function accessImmutableObject (object, array) {
   return fromJS(object).getIn(array, undefined);
-  //return getIn(object, array, undefined);
+  // return getIn(object, array, undefined);
 }
