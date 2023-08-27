fda41cf27e9ba44e7b590969189c2d78b5535ee9
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-08-27 06:31:49 +0100

    Update 5-merge.js

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -5,5 +5,5 @@ export const concatElements = (page1, page2) => {
 };
 
 export const mergeElements = (page1, page2) => {
-  return List(Map(page1).merge(Map(page2)));
+  return Map(page1).merge(Map(page2));
 };
